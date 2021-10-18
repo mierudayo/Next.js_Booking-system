b37c7c64f025b9787f6d6d267841f609ed5fbba8
hidetomo0102 <hidesakura23>
2021-10-18 20:51:29 +0900

    add api methods

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,13 +1,26 @@
 import Room from "../models/room";
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncError from "../middlewares/catchAsyncError";
+import APIFeatures from "../utils/apiFeatures";
 
 // Get All Rooms => (GET) /api/rooms/
 export const getAllRooms = catchAsyncError(async (req, res) => {
-  const rooms = await Room.find();
+  const resPerPage = 4;
+  const roomsCount = await Room.countDocuments();
+
+  const apiFeatures = new APIFeatures(Room.find(), req.query).search().filter();
+
+  let rooms = await apiFeatures.query;
+  let filteredRoomsCount = rooms.length;
+
+  apiFeatures.pagination(resPerPage);
+  rooms = await apiFeatures.query.clone();
+
   res.status(200).json({
     success: true,
-    count: rooms.length,
+    roomsCount,
+    resPerPage,
+    filteredRoomsCount,
     rooms,
   });
 });
