37317a0c83864f8a871f014aab597f3b91886fb4
hidetomo0102 <hidenorishio@icloud.com>
2021-10-29 14:55:34 +0900

    fix properties

--- components/user/ForgotPassword.jsx
+++ components/user/ForgotPassword.jsx
@@ -33,16 +33,16 @@ export const ForgotPassword = () => {
   };
 
   return (
-    <div class="row wrapper">
-      <div class="col-10 col-lg-5">
-        <form class="shadow-lg" onSubmit={submitHandler}>
-          <h1 class="mb-3">Forgot Password</h1>
-          <div class="form-group">
+    <div className="row wrapper">
+      <div className="col-10 col-lg-5">
+        <form className="shadow-lg" onSubmit={submitHandler}>
+          <h1 className="mb-3">Forgot Password</h1>
+          <div className="form-group">
             <label htmlFor="email_field">Enter Email</label>
             <input
               type="email"
               id="email_field"
-              class="form-control"
+              className="form-control"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
@@ -51,7 +51,7 @@ export const ForgotPassword = () => {
           <button
             id="forgot_password_button"
             type="submit"
-            class="btn btn-block py-3"
+            className="btn btn-block py-3"
             disabled={loading}
           >
             {loading ? <ButtonLoader /> : "Send Email"}
