6279addda460d5b6f1524e934ecbe845e15cd94c
hidetomo0102 <hidenorishio@icloud.com>
2021-11-05 14:11:56 +0900

    add 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,10 @@
+import { Layout } from "../components/layouts/Layout";
+import { NotFound } from "../components/layouts/NotFound";
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Not Found">
+      <NotFound />
+    </Layout>
+  );
+}
