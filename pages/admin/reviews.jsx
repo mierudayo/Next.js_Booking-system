22f77c0765d6ed75b99170a57d765957916635eb
hidetomo0102 <hidenorishio@icloud.com>
2021-11-12 12:14:21 +0900

    fix lint

--- pages/admin/reviews.jsx
+++ pages/admin/reviews.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { getSession } from "next-auth/client";
 
-import { Layout } from "../../components/layout/Layout";
 import { RoomReviews } from "../../components/admin/RoomReviews";
+import { Layout } from "../../components/layouts/Layout";
 
 const RoomReviewsPage = () => {
   return (
